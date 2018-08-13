9e89e804965fd74bb63350c3c1e9fe968fc18d0e
Matthew D Rainville <mrainville@eastbay.com>
2018-08-13 15:55:23 -0500

    add base pages

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Row from 'components/common/layout/row';
+
+class Contact extends React.Component {
+	render() {
+		return (
+			<Row>
+				Contact me at <a href="mailto:mrainville65@gmail.com">mrainville65@gmail.com</a>
+			</Row>
+		)
+	}
+};
+
+export default Contact;
